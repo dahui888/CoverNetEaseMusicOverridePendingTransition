a9cd75bc780ea3a2da6eafca4a56dffe7b5b0265
negier <1208883805@qq.com>
2017-11-04 17:57:45 +0800

    Initial commit

---
+++ app/src/main/java/com/negier/coverneteasemusicoverridependingtransition/SplashAnimTwoActivity.java
@@ -0,0 +1,14 @@
+package com.negier.coverneteasemusicoverridependingtransition;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashAnimTwoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_anim_two);
+    }
+}
